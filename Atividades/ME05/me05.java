baf732999d36cd85519c4532c293cfb6b02f76fb
Pedro Teixeira <129986058+pedroteixeira02@users.noreply.github.com>
2024-06-11 20:45:53 -0300

    Add files via upload

---
+++ Atividades/ME05/me05.java
@@ -0,0 +1,98 @@
+
+package animals;
+
+public abstract class Animal {
+    public abstract void fazerBarulho();
+}
+
+package interfaces;
+
+public interface Colorido {
+    String obterCor();
+}
+
+package interfaces;
+
+public interface Colorido {
+    String obterCor();
+}
+--------------------------------------------------------------------------------------------
+
+package animals;
+
+public abstract class Animal {
+    public abstract void fazerBarulho();
+}
+
+
+package interfaces;
+
+public interface Pesado {
+    double obterPeso();
+}
+
+package interfaces;
+
+public interface Colorido {
+    String obterCor();
+}
+
+package animals;
+
+import interfaces.Pesado;
+import interfaces.Colorido;
+
+public class Porco extends Animal implements Pesado, Colorido {
+    @Override
+    public void fazerBarulho() {
+        System.out.println("Oinc!");
+    }
+
+    @Override
+    public double obterPeso() {
+        return 50.0;
+    }
+
+    @Override
+    public String obterCor() {
+        return "Preto";
+    }
+}
+---------------------------------------------------------------------------------------
+
+package animals;
+
+import interfaces.Pesado;
+import interfaces.Colorido;
+
+public class Porco extends Animal implements Pesado, Colorido {
+    @Override
+    public void fazerBarulho() {
+        System.out.println("Oinc!");
+    }
+
+    @Override
+    public double obterPeso() {
+        return 50.0;
+    }
+
+    @Override
+    public String obterCor() {
+        return "Preto";
+    }
+
+
+    public boolean enlameado() {
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return String.format(
+            "\nCor: %s\nPeso: %.2f\nEnlameado? %s",
+            obterCor(),
+            obterPeso(),
+            enlameado() ? "Sim" : "Não"
+        );
+    }
+}
